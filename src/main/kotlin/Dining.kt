033f792b82edc846f35a6bf2cc4a793e484c191a
Swapnil Patel <sp70215@gmail.com>
2020-05-07 17:38:39 -0700

    Remove `Dining` class error handling
    Library should not worry about error handling for these functions as implementation should be up to the user.
    
    Added hinting decorators for possible thrown exceptions.

--- src/main/kotlin/Dining.kt
+++ src/main/kotlin/Dining.kt
@@ -1,3 +1,4 @@
+import com.sun.javaws.exceptions.InvalidArgumentException
 import models.DiningHallType
 import models.DiningMenu
 import models.HallMenu
@@ -5,32 +6,27 @@ import parser.MenuParser
 import java.io.IOException
 import java.util.*
 
+/**
+ *  Front-end of library that wraps around the internal [MenuParser] class
+ */
 class Dining {
     companion object {
         @JvmStatic
         val menuParser: MenuParser = MenuParser()
     }
 
-    fun getDiningMenu(date: Date): DiningMenu? {
-        return kotlin.runCatching {
-            menuParser.getDiningMenu(date)
-        }.getOrElse {
-            println(it.message)
-            null
-        }
+    @Throws(IOException::class, InvalidArgumentException::class)
+    fun getDiningMenu(date: Date): DiningMenu {
+        return menuParser.getDiningMenu(date)
     }
 
-    fun getHallMenu(diningHall: DiningHallType, date: Date): HallMenu? {
-        return kotlin.runCatching {
-            val diningMenu = menuParser.getDiningMenu(date)
-            when (diningHall) {
-                DiningHallType.PARKSIDE -> diningMenu.parkside
-                DiningHallType.EVK -> diningMenu.evk
-                DiningHallType.VILLAGE -> diningMenu.village
-            }
-        }.getOrElse {
-            println(it.message)
-            null
+    @Throws(IOException::class, InvalidArgumentException::class)
+    fun getHallMenu(diningHall: DiningHallType, date: Date): HallMenu {
+        val diningMenu = menuParser.getDiningMenu(date)
+        return when (diningHall) {
+            DiningHallType.PARKSIDE -> diningMenu.parkside
+            DiningHallType.EVK -> diningMenu.evk
+            DiningHallType.VILLAGE -> diningMenu.village
         }
     }
 }
