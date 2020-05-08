bad35b6c93f94f787d006b013d528e2c5bc3921b
Swapnil Patel <sp70215@gmail.com>
2020-05-07 19:51:59 -0700

    Update build.gradle.kts for use with JitPack
    Added the maven-publish plugin so JitPack can build this properly.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,18 @@ val kotlinVersion = "1.3.71"
 val jsoupVersion = "1.13.1"
 
 plugins {
+    // Apply the java-library plugin for API and implementation separation.
+    java
+
+    // Apply the maven publish plugin
+    maven
+
     // Apply the Kotlin JVM plugin to add support for Kotlin.
     kotlin("jvm") version "1.3.71"
 
     // Apply Dokka plugin for documentation
     id("org.jetbrains.dokka") version "0.10.0"
 
-    // Apply the java-library plugin for API and implementation separation.
-    `java-library`
 }
 
 group = "me.switchswap"
@@ -36,10 +40,10 @@ java {
 }
 
 repositories {
+    mavenCentral()
     // Use jcenter for resolving dependencies.
     // You can declare any Maven/Ivy/file repository here.
     jcenter()
-    mavenCentral()
 }
 
 dependencies {
