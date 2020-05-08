a5b8dbcdbabd73f43070efe0df34c2b04584f705
Swapnil Patel <sp70215@gmail.com>
2020-05-07 18:59:29 -0700

    Add Dokka plugin to Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.jvm.tasks.Jar
+import org.jetbrains.dokka.gradle.DokkaTask
+
 val kotlinVersion = "1.3.71"
 val jsoupVersion = "1.13.1"
 
@@ -5,8 +8,24 @@ plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
     kotlin("jvm") version "1.3.71"
 
+    // Apply Dokka plugin for documentation
+    id("org.jetbrains.dokka") version "0.10.0"
+
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
+}
+
+
+tasks.dokka {
+    outputFormat = "html"
+    outputDirectory = "$buildDir/javadoc"
+}
+
+val dokkaJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles Kotlin docs with Dokka"
+    classifier = "javadoc"
+    from(tasks.dokka)
 }
 
 java {
